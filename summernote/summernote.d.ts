c2732e4c63060d591539ad29ce205a0b07911ba3
wstaelens <wstaelens@users.noreply.github.com>
2016-11-24 17:47:52 +0100

    initial commit to allow to build my asp.net mvc solution with TypeScript files
    had references to summernote which did not allow me to build my .ts files.
    Created first file to support summernote so that build succeeded. Needs more work!

---
+++ summernote/summernote.d.ts
@@ -0,0 +1,29 @@
+﻿// Type definitions for Summernote v0.8.2
+// Project: https://github.com/summernote/summernote/ http://summernote.org/
+// Definitions by: Wouter Staelens (Winking - http://www.winking.be)
+// Definitions: https://github.com/wstaelens/TypeScript-Summernote
+
+
+/// <reference path="../jquery/jquery.d.ts"/>
+
+interface SummernoteOptions {
+    height?: number;
+    minHeight?: number;
+    maxHeight?: number;
+    focus?: boolean;
+    toolbar?: any;
+    popover?: any;
+    callbacks?: any
+}
+
+interface JQuery {
+    summernote(): JQuery;
+    summernote(command: string): JQuery;
+    summernote(options?: SummernoteOptions): JQuery;
+    summernote(command: string, markupString: string): JQuery
+}
+
+declare module "summernote" {
+}
+
+// http://summernote.org/deep-dive/#api
