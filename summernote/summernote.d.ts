37dfbaa03ee489f335b743b813f9cc3ea46f9b3a
wstaelens <wstaelens@users.noreply.github.com>
2016-11-28 11:49:21 +0100

    updates to definition

--- summernote/summernote.d.ts
+++ summernote/summernote.d.ts
@@ -7,28 +7,114 @@
 /// <reference path="../jquery/jquery.d.ts"/>
 
 interface SummernoteOptions {
-    height?: number;
-    minHeight?: number;
-    maxHeight?: number;
-    focus?: boolean;
-    //toolbar?: any;
-    toolbar?: ToolbarOptions;
-    popover?: any;
-    callbacks?: any;
+    airMode?: boolean;
+    callbacks?: any; // todo
+    codemirror?: CodemirrorOptions;
+    colors?: colorsDef;
     dialogsInBody?: boolean;
     dialogsFade?: boolean;
+    direction?: string;
     disableDragAndDrop?: boolean;
-    shortcuts?: boolean;
+    focus?: boolean;
     fontNames?: string[];
     fontNamesIgnoreCheck?: string[];
-    placeholder?: string;
+    height?: number;
+    hint?: HintOptions;
+    icons?: IconsOptions;
+    insertTableMaxSize?: InsertTableMaxSizeOptions;
+    keyMap?: KeyMapOptions;
+    lang?: string;
+    lineHeights?: string[];
+    minHeight?: number;
+    maxHeight?: number;
+    maximumImageFileSize?: any;
     modules?: ModuleOptions;
+    popover?: PopoverOptions;
+    placeholder?: string;
+    shortcuts?: boolean;
+    styleTags?: styleTagsOptions[];
+    styleWithSpan?: boolean;
+    tabsize?: number;
+    tableClassName?: string;
+    textareaAutoSync?: boolean;
+    toolbar?: toolbarDef;
+    width?: number;
+}
+
+//type toolbarDef = [string, string[]][];
+type toolbarStyleGroupOptions = 'style' | 'bold' | 'italic' | 'underline' | 'clear';
+type toolbarFontGroupOptions = 'strikethrough' | 'superscript' | 'subscript';
+type toolbarFontsizeGroupOptions = 'fontsize';
+type toolbarColorGroupOptions = 'color';
+type toolbarParaGroupOptions = 'ul' | 'ol' | 'paragraph';
+type toolbarHeightGroupOptions = 'height';
+type toolbarTableGroupOptions = 'table';
+type toolbarInsertGroupOptions = 'link' | 'picture' | 'hr';
+type toolbarViewGroupOptions = 'fullscreen' | 'codeview';
+type toolbarHelpGroupOptions = 'help';
+type toolbarDef = [
+    ['style', toolbarStyleGroupOptions[]]
+    | ['font', toolbarFontGroupOptions[]]
+    | ['fontsize', toolbarFontsizeGroupOptions[]]
+    | ['color', toolbarColorGroupOptions[]]
+    | ['para', toolbarParaGroupOptions[]]
+    | ['height', toolbarHeightGroupOptions[]]
+    | ['table', toolbarTableGroupOptions[]]
+    | ['insert', toolbarInsertGroupOptions[]]
+    | ['view', toolbarViewGroupOptions[]]
+    | ['help', toolbarHelpGroupOptions[]]
+];
+
+type colorsDef = [string[]][];
+type styleTagsOptions = 'p' | 'blockquote' | 'pre' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+
+interface InsertTableMaxSizeOptions {
+    col: number;
+    row: number;
+}
+
+interface IconsOptions {
+    options?: any; // todo
+}
+
+interface KeyMapOptions {
+    pc?: KeyMapPcOptions;
+    mac?: KeyMapMacOptions;
+}
+
+interface KeyMapPcOptions {
+    options?: any; // todo
+}
+
+interface KeyMapMacOptions {
+    options?: any; // todo
+}
+
+interface HintOptions {
+    words?: string[];
+    mentions?: string[];
+    match: RegExp;
+    search: Function;
+    template?: Function;
+    content?: Function;
+}
+
+interface CodemirrorOptions {
+    mode?: string;
+    htmlNode?: boolean;
+    lineNumbers?: boolean;
+    theme?: string;
+}
+
+interface PopoverOptions {
+    image?: any; // todo
+    link?: any; // todo
+    air?: any; // todo
 }
 
 interface ToolbarOptions {
     options?: any; // todo
 }
-
 
 
 interface ModuleOptions {
@@ -38,9 +124,6 @@ interface ModuleOptions {
 interface CreateLinkOptions {
     text: string;
     url: string;
-    /**
-    * whether Link's target is new window or not
-    */
     newWindow: boolean;
 }
 
@@ -52,40 +135,44 @@ interface JQuery {
     summernote(command: string, value: number): JQuery;
     summernote(command: string, node: Node): JQuery;
     summernote(command: string, url: string, filename?: (string | Function)): JQuery;
-    //summernote(method: "test"): JQuery;
+
+    summernote(command: 'destroy'): JQuery;
+    summernote(command: 'code', markupStr?: string): JQuery;
+    summernote(command: 'editor.pasteHTML' | 'pasteHTML', markup : string): JQuery;
+
     // Basic API
-    summernote(command: 'createRange'): JQuery;
-    summernote(command: 'saveRange'): JQuery;
-    summernote(command: 'restoreRange'): JQuery;
-    summernote(command: 'undo'): JQuery;
-    summernote(command: 'redo'): JQuery;
-    summernote(command: 'focus'): JQuery;
-    summernote(command: 'isEmpty'): boolean;
+    summernote(command: 'editor.createRange' | 'createRange'): JQuery;
+    summernote(command: 'editor.saveRange' | 'saveRange'): JQuery;
+    summernote(command: 'editor.restoreRange' | 'restoreRange'): JQuery;
+    summernote(command: 'editor.undo' | 'undo'): JQuery;
+    summernote(command: 'editor.redo' | 'redo'): JQuery;
+    summernote(command: 'editor.focus' | 'focus'): JQuery;
+    summernote(command: 'editor.isEmpty' | 'isEmpty'): boolean;
     summernote(command: 'reset'): JQuery;
     summernote(command: 'disable'): JQuery;
     summernote(command: 'enable'): JQuery;
     // Font style API
-    summernote(fontStyle: 'bold'): JQuery;
-    summernote(fontStyle: 'italic'): JQuery;
-    summernote(fontStyle: 'underline'): JQuery;
-    summernote(fontStyle: 'strikethrough'): JQuery;
-    summernote(command: 'superscript'): JQuery;
-    summernote(command: 'subscript'): JQuery;
-    summernote(command: 'removeFormat'): JQuery;
+    summernote(fontStyle: 'editor.bold' | 'bold'): JQuery;
+    summernote(fontStyle: 'editor.italic' | 'italic'): JQuery;
+    summernote(fontStyle: 'editor.underline' | 'underline'): JQuery;
+    summernote(fontStyle: 'editor.strikethrough' | 'strikethrough'): JQuery;
+    summernote(command: 'editor.superscript' | 'superscript'): JQuery;
+    summernote(command: 'editor.subscript' | 'subscript'): JQuery;
+    summernote(command: 'editor.removeFormat' | 'removeFormat'): JQuery;
     summernote(command: 'backColor', color: string): JQuery;
     summernote(command: 'foreColor', color: string): JQuery;
     summernote(command: 'fontName', fontName: string): JQuery;
-    summernote(command: 'fontSize', fontSize: number): JQuery;
+    summernote(command: 'editor.fontSize' | 'fontSize', fontSize: number): JQuery;
     // Paragraph API
-    summernote(command: 'justifyLeft'): JQuery;
-    summernote(command: 'justifyRight'): JQuery;
-    summernote(command: 'justifyCenter'): JQuery;
-    summernote(command: 'justifyFull'): JQuery;
+    summernote(command: 'editor.justifyLeft' | 'justifyLeft'): JQuery;
+    summernote(command: 'editor.justifyRight' | 'justifyRight'): JQuery;
+    summernote(command: 'editor.justifyCenter' | 'justifyCenter'): JQuery;
+    summernote(command: 'editor.justifyFull' | 'justifyFull'): JQuery;
     summernote(command: 'insertParagraph'): JQuery;
-    summernote(command: 'insertOrderedList'): JQuery;
-    summernote(command: 'insertUnorderedList'): JQuery;
-    summernote(command: 'indent'): JQuery;
-    summernote(command: 'outdent'): JQuery;
+    summernote(command: 'editor.insertOrderedList' | 'insertOrderedList'): JQuery;
+    summernote(command: 'editor.insertUnorderedList' | 'insertUnorderedList'): JQuery;
+    summernote(command: 'editor.indent' | 'indent'): JQuery;
+    summernote(command: 'editor.outdent' | 'outdent'): JQuery;
     summernote(command: 'formatPara'): JQuery;
     summernote(command: 'formatH1'): JQuery;
     summernote(command: 'formatH2'): JQuery;
@@ -94,13 +181,31 @@ interface JQuery {
     summernote(command: 'formatH5'): JQuery;
     summernote(command: 'formatH6'): JQuery;
     // Insertion API
-    summernote(command: 'insertImage', url: string, filename?: (string | Function)): JQuery;
-    summernote(command: 'insertNode', node: Node): JQuery;
-    summernote(command: 'insertText', text: string): JQuery;
+    summernote(command: 'editor.insertImage' | 'insertImage', url: string, filename?: (string | Function)): JQuery;
+    summernote(command: 'editor.insertNode' | 'insertNode', node: Node): JQuery;
+    summernote(command: 'editor.insertText' | 'insertText', text: string): JQuery;
     summernote(command: 'createLink', options: CreateLinkOptions): JQuery;
-    summernote(command: 'unlink'): JQuery;
+    summernote(command: 'editor.unlink' | 'unlink'): JQuery;
     // Callbacks
     // todo!
+
+    // what about these?
+    // editor.insertImagesOrCallback ??
+    // editor.afterCommand ??
+    // editor.resizeTo ??
+    // editor.resize ??
+    // editor.saveTarget ??
+    // editor.isActivated ??
+    // editor.formatBlock ??
+    // editor.color ??
+    // editor.lineHeight ??
+    // editor.insertTable ??
+    // editor.insertHorizontalRule ??
+    // editor.floatMe ??
+    // editor.removeMedia ??
+    // editor.currentStyle ??
+    // editor.getLinkInfo ??
+    // editor.getSelectedText ??
 }
 
 declare module "summernote" {
